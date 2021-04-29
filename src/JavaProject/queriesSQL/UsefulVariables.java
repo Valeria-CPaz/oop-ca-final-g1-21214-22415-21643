a382e9be3273fb7fb634b97b93d0e8a080865330
Valeria Paz <21214@student.dorset-college.ie>
2021-04-29 21:49:39 +0100

    4th commit - Val

---
+++ src/JavaProject/queriesSQL/UsefulVariables.java
@@ -0,0 +1,77 @@
+package JavaProject.queriesSQL;
+
+import java.sql.PreparedStatement;
+
+public class UsefulVariables {
+
+    public final static String URL = "jdbc:mysql://127.0.0.1:3306/collegeManagementSystem?autoReconnect=true&useSSL=false";
+    public final static String DB_USER = "root";
+    public final static String DB_PASSWORD = "M@rcus2020";
+
+    public static PreparedStatement createBranchQuery;
+
+    public static PreparedStatement createAssignmentQuery;
+
+
+
+    public static PreparedStatement getBranches;
+
+    public static PreparedStatement getCourses;
+
+    public static PreparedStatement getLecturersId;
+
+    public static PreparedStatement getModules;
+
+    public static PreparedStatement createModuleGetCourses;
+
+    public static PreparedStatement getAllStudents;
+
+    public static PreparedStatement getAllLecturers;
+
+    public static PreparedStatement getAllCourses;
+
+    public static PreparedStatement getAllBranches;
+
+    public static PreparedStatement getAllModules;
+
+    public static PreparedStatement getAllCourseYear;
+
+    public static PreparedStatement getPayments;
+
+    public static PreparedStatement addPaymentQuery;
+
+
+    public static PreparedStatement getAllAssignments;
+
+    public static PreparedStatement getSelectedAssignment;
+
+    public static PreparedStatement editAssignment;
+
+    public static PreparedStatement deleteAssignment;
+
+    public static PreparedStatement addGrade;
+
+    public static PreparedStatement getSelectedGrade;
+
+    public static PreparedStatement getAllGrades;
+
+    public static PreparedStatement createExam;
+
+    public static PreparedStatement editExam;
+
+    public static PreparedStatement deleteExam;
+
+    public static PreparedStatement getSelectedExam;
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/JavaProject/tableviewPOJO/CollegeBranchMaster.java
@@ -0,0 +1,36 @@
+package JavaProject.tableviewPOJO;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class CollegeBranchMaster {
+
+    private final SimpleStringProperty branchUnit = new SimpleStringProperty();
+    private final SimpleStringProperty address = new SimpleStringProperty();
+
+    public CollegeBranchMaster() {
+    }
+
+    public String getBranchUnit() {
+        return branchUnit.get();
+    }
+
+    public SimpleStringProperty branchUnitProperty() {
+        return branchUnit;
+    }
+
+    public void setBranchUnit(String branchUnit) {
+        this.branchUnit.set(branchUnit);
+    }
+
+    public String getAddress() {
+        return address.get();
+    }
+
+    public SimpleStringProperty addressProperty() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address.set(address);
+    }
+}
